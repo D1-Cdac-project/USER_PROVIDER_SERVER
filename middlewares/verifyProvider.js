cdf1db15076b03684fc40b463165ad4cfd107cbf
Akshay Londhe <akshaylondhe3399@gmail.com>
2025-06-08 17:18:39 +0530

    fix: added object in findOne and added provider existance check

--- middlewares/verifyProvider.js
+++ middlewares/verifyProvider.js
@@ -1,13 +1,19 @@
 const jwt = require("jsonwebtoken");
 const providerModal = require("../models/providerModel");
+
 exports.isProvider = async (req, res, next) => {
   try {
     const { providerToken } = req.cookies;
     if (!providerToken) {
-      return res.status(500).json({ success: false });
+      return res.status(401).json({ success: false, message: "No token provided" });
     }
-    const decodeData = jwt.verify(providerToken, process.env.SECRET_KEY);
-    req.provider = await providerModal.findOne(decodeData.id);
+    const decodedData = jwt.verify(providerToken, process.env.SECRET_KEY);
+
+    const provider = await providerModal.findOne({ _id: decodedData.id });
+    if(!provider){
+      return res.status(404).json({ success : false, message : "Provider not found"})
+    }
+    req.provider = provider;
     next();
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
