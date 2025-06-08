c7959d9e2049a0ca9a211282098e5d007b772ea3
Akshay Londhe <akshaylondhe3399@gmail.com>
2025-06-08 17:30:30 +0530

    Update: added cookie-parser to enable cookie access

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const env = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const userRouter = require("./routes/userRoutes");
 const providerRouter = require("./routes/providerRoutes")
@@ -12,6 +13,7 @@ mongoose.set("strictQuery", true);
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5500", "http://localhost:3000"],
