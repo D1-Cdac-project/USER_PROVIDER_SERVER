dad85d97e2735fc7148c93b27012ead4eb1c2f59
Tanay Phadke <tanphadke210@gmail.com>
2025-06-08 09:13:10 +0530

    Adding provider controllers

---
+++ controllers/providerController.js
@@ -0,0 +1,30 @@
+//related to provider
+exports.registerProvider = async (req, res) => {}
+exports.loginProvider = async (req, res) => {}
+exports.getProvider = async (req, res) => {}
+exports.updateProvider = async (req, res) => {}
+
+//related to booking
+exports.getAllBookings = async (req, res) => {}
+
+//Related to mandap
+exports.createMandap = async (req, res) => {}
+getAllMandapByProviderID = async (req, res) => {}
+exports.updateMandap = async (req, res) => {}
+exports.deleteMandap = async (req, res) => {}
+exports.searchProviderMandap = async (req, res) => {}
+
+//room related
+exports.addRoom = async (req, res) => {}
+exports.updateRoom = async (req , res) =>{}
+exports.deleteRoom = async (req, res) => {}
+
+//Photographer related
+exports.addPhotographer = async (req, res) => {}
+exports.updatePhotographer = async (req , res) =>{}
+exports.deletePhotographer = async (req, res) => {}
+
+//caterer related
+exports.addCaterer = async (req, res) => {}
+exports.updateCaterer = async (req , res) =>{}
+exports.deleteCaterer = async (req, res) => {}
