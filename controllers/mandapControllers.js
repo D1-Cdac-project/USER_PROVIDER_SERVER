dd647c9102fb3a7f432098cbb2ee8fab65a1d533
Tanay Phadke <tanphadke210@gmail.com>
2025-06-08 09:17:55 +0530

    adding mandapControllers

---
+++ controllers/mandapControllers.js
@@ -0,0 +1,24 @@
+//mandap related
+exports.getAllMandaps = async (req, res) => {}
+exports.searchMandap = async (req, res) => {}
+exports.getMandapByFilter = async (req, res) => {}
+exports.getMandapByID = async (req, res) => {}
+exports.checkAvailability = async (req, res) => {}
+
+//caterer related
+exports.getCatererById = async (req, res) => {}
+exports.getAllCaterersByMandapId = async (req, res) => {}
+
+//review related
+exports.getReviewById = async (req, res) => {}
+
+//photographer related
+exports.getPhotographerByMandapId = async (req, res) => {}
+exports.getAllPhotographers = async (req, res) => {}
+
+//room related
+exports.getRoomByMandapId = async (req, res) => {}
+exports.getAllRooms = async (req, res) => {}
+
+//optional
+exports.getFeaturedMandaps = async (req, res) => {}
