ecd3bf04ead62da066e5e537f9553c022378a03f
Kunal Hulke <hulke1102002@gmail.com>
2025-06-12 23:11:46 +0530

    updating the provider routes

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -1,14 +1,20 @@
-const express = require('express')
+const express = require("express");
 
-const {isProvider} = require('../middlewares/verifyProvider')
-const {registerProvider, loginProvider, logoutProvider, getProvider, updateProvider} = require('../controllers/providerControllers')
+const { isProvider } = require("../middlewares/verifyProvider");
+const {
+  registerProvider,
+  loginProvider,
+  logoutProvider,
+  getProvider,
+  updateProvider,
+} = require("../controllers/providerControllers");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', registerProvider)
-router.post('/login', loginProvider)
-router.post('/logout', logoutProvider)
-router.get('/me',isProvider, getProvider)
-router.put('/update', isProvider, updateProvider)
+router.post("/signup", registerProvider);
+router.post("/login", loginProvider);
+router.post("/logout", logoutProvider);
+router.get("/profile", isProvider, getProvider);
+router.put("/update-profile", isProvider, updateProvider);
 
-module.exports = router
+module.exports = router;
