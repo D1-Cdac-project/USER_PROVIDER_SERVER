37809fe739bfdeb00e4fbd91468dca13be1870ee
Tanay Phadke <tanphadke210@gmail.com>
2025-06-12 23:24:14 +0530

    Adding provider related mandap apis

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -1,14 +1,22 @@
 const express = require('express')
 
 const {isProvider} = require('../middlewares/verifyProvider')
-const {registerProvider, loginProvider, logoutProvider, getProvider, updateProvider} = require('../controllers/providerControllers')
+const {registerProvider, loginProvider, logoutProvider, getProvider, updateProvider,
+    createMandap , getAllMandapByProviderID, updateMandap, deleteMandap} = require('../controllers/providerControllers')
 
 const router = express.Router()
 
+// Provider routes
 router.post('/register', registerProvider)
 router.post('/login', loginProvider)
 router.post('/logout', logoutProvider)
 router.get('/me',isProvider, getProvider)
 router.put('/update', isProvider, updateProvider)
+
+// Mandap routes
+router.post('/mandap' , isProvider, createMandap)
+router.get('/getmandap', isProvider, getAllMandapByProviderID)
+router.put('/updatemandap/:mandapid', isProvider, updateMandap)
+router.delete('/deletemandap/:mandapid', isProvider, deleteMandap)
 
 module.exports = router
