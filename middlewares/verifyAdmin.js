194ca2b0b795a627c33cde00987d088e84f27337
Kunal Hulke <hulke1102002@gmail.com>
2025-06-12 23:11:29 +0530

    adding: 1. Admin routes 2. Admin Controller 3. AdminModel 4. verifying admin is present or not

---
+++ middlewares/verifyAdmin.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const adminModel = require("../models/adminModel");
+
+exports.isAdmin = async (req, res, next) => {
+  try {
+    const { adminToken } = req.cookies;
+    if (!adminToken) return res.status(500).json({ success: false });
+
+    const decodeData = jwt.verify(adminToken, process.env.SECRET_KEY);
+    req.user = await adminModel.findById(decodeData.id);
+    next();
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
