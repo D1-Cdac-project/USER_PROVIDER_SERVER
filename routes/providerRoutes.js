339fdc73084d9c799be8ea44f1ef9b5ad07215e2
Kunal Hulke <hulke1102002@gmail.com>
2025-06-13 18:37:01 +0530

    refactor the code

--- routes/providerRoutes.js
+++ routes/providerRoutes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 
-const {isProvider} = require('../middlewares/verifyProvider')
-const {registerProvider, loginProvider, logoutProvider, getProvider, updateProvider,
-    createMandap , getAllMandapByProviderID, updateMandap, deleteMandap} = require('../controllers/providerControllers')
+const { isProvider } = require("../middlewares/verifyProvider");
+const {
+  registerProvider,
+  loginProvider,
+  logoutProvider,
+  getProvider,
+  updateProvider,
+  createMandap,
+  getAllMandapByProviderID,
+  updateMandap,
+  deleteMandap,
+} = require("../controllers/providerControllers");
 
-const router = express.Router()
+const router = express.Router();
 
+// Provider routes
 router.post("/signup", registerProvider);
 router.post("/login", loginProvider);
 router.post("/logout", logoutProvider);
@@ -13,9 +23,9 @@ router.get("/profile", isProvider, getProvider);
 router.put("/update-profile", isProvider, updateProvider);
 
 // Mandap routes
-router.post('/mandap' , isProvider, createMandap)
-router.get('/getmandap', isProvider, getAllMandapByProviderID)
-router.put('/updatemandap/:mandapid', isProvider, updateMandap)
-router.delete('/deletemandap/:mandapid', isProvider, deleteMandap)
+router.post("/mandap", isProvider, createMandap);
+router.get("/getmandap", isProvider, getAllMandapByProviderID);
+router.put("/updatemandap/:mandapid", isProvider, updateMandap);
+router.delete("/deletemandap/:mandapid", isProvider, deleteMandap);
 
 module.exports = router;
