66c7c83f7f3032d1882008c1ea3b31073af34a4d
Kunal Hulke <hulke1102002@gmail.com>
2025-07-22 23:58:39 +0530

    seperating the controller to respective files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,41 +5,80 @@ const {
   loginUser,
   getUserDetails,
   logoutUser,
-  getAllFavoriteMandaps,
-  addFavoriteMandap,
-  deleteFavoriteMandap,
+  getCatererById,
+  getAllCaterersByMandapId,
+  getReviewById,
+  getPhotographerByMandapId,
+  getAllPhotographers,
+  getRoomByMandapId,
+  getAllRooms,
+  getFeaturedMandaps,
+} = require("../controllers/userControllers");
+const { isUser } = require("../middlewares/verifyUser");
+const { upload } = require("../config/cloudinary");
+const {
   addBooking,
   getAllBookings,
   getBookingById,
   deleteBooking,
   updateBooking,
-} = require("../controllers/userControllers");
-const { isUser } = require("../middlewares/verifyUser");
+} = require("../controllers/bookingController");
+const {
+  addFavoriteMandap,
+  getAllFavoriteMandaps,
+  deleteFavoriteMandap,
+} = require("../controllers/favouriteMandapController");
+const {
+  addReview,
+  updateReviewById,
+  deleteReviewById,
+} = require("../controllers/reviewController");
+const {
+  getAllMandaps,
+  searchMandap,
+  getMandapByFilter,
+  getMandapByID,
+} = require("../controllers/mandapControllers");
 
-// Initialize router
 const router = express.Router();
 
-// User authentication routes
 router.post("/signup", (req, res) => registerUser(req, res, req.io));
 router.post("/login", (req, res) => loginUser(req, res, req.io));
 router.post("/logout", logoutUser);
-
-// User profile routes
 router.get("/profile", isUser, getUserDetails);
-router.put("/update-profile", isUser, updateProfile);
-
-// Favorite mandap route
+router.put(
+  "/update-profile",
+  isUser,
+  upload.single("profileImage"),
+  updateProfile
+);
 router.post("/add-favorite-mandap", isUser, addFavoriteMandap);
-router.put("/update-profile", updateProfile);
-
-router.post("/add-booking", isUser, addBooking)
-router.get("/bookings", isUser, getAllBookings)
-router.get("/booking/:id", isUser, getBookingById)
-router.delete("/delete/:id", isUser, deleteBooking)
-router.put("/update/:id", isUser, updateBooking)
-
 router.get("/favourite-mandaps", isUser, getAllFavoriteMandaps);
-router.post("/favourite-mandap", isUser, addFavoriteMandap);
 router.delete("/favourite-mandap/:mandapId", isUser, deleteFavoriteMandap);
+
+router.post("/add-booking", isUser, addBooking);
+router.get("/bookings", isUser, getAllBookings);
+router.get("/booking/:id", isUser, getBookingById);
+router.delete("/delete/:id", isUser, deleteBooking);
+router.put("/update/:id", isUser, updateBooking);
+
+router.get("/mandaps", getAllMandaps);
+router.get("/search-mandap", searchMandap);
+router.post("/filter-mandap", getMandapByFilter);
+router.get("/mandap/:mandapId", getMandapByID);
+
+router.get("/caterer/:catererId", getCatererById);
+router.get("/caterers/:mandapId", getAllCaterersByMandapId);
+
+router.get("/photographers/:mandapId", getPhotographerByMandapId);
+router.get("/photographers", getAllPhotographers);
+
+router.get("/rooms/:mandapId", getRoomByMandapId);
+router.get("/rooms", getAllRooms);
+
+router.post("/add-review", isUser, addReview);
+router.get("/review/:reviewId", isUser, getReviewById);
+router.put("/update-review/:reviewId", isUser, updateReviewById);
+router.delete("/delete-review/:reviewId", isUser, deleteReviewById);
 
 module.exports = router;
