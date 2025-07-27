cfb2ec988ce2ea67fe3c8596899ddf90ca981ee1
krh1102002 <hulke1102002@gmail.com>
2025-07-27 18:21:24 +0530

    added createdAt and timeStamp into the reveiwModel,

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -1,37 +1,40 @@
 const mongoose = require("mongoose");
 
 // Notification schema definition
-const notificationSchema = new mongoose.Schema({
-  recipientId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Admin",
-    required: true,
+const notificationSchema = new mongoose.Schema(
+  {
+    recipientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["provider_registration", "user_registration", "approval_request"],
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    relatedId: {
+      type: mongoose.Schema.Types.ObjectId,
+      refPath: "relatedModel",
+    },
+    relatedModel: {
+      type: String,
+      enum: ["Providers", "Users", "ApprovalRequests", "Bookings", "Mandaps"],
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  type: {
-    type: String,
-    enum: ["provider_registration", "user_registration", "approval_request"],
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  relatedId: {
-    type: mongoose.Schema.Types.ObjectId,
-    refPath: "relatedModel",
-  },
-  relatedModel: {
-    type: String,
-    enum: ["Providers", "Users", "ApprovalRequests", "Bookings", "Mandaps"],
-  },
-  isRead: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Notifications", notificationSchema);
