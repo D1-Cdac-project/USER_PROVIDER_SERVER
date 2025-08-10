bbf6a9280b4f47260b47312508428cdc4ae05718
Kunal Raju Hulke <hulke1102002@gmail.com>
2025-08-11 00:05:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ app.use(
       "http://127.0.0.1:5174",
       "http://127.0.0.1:5175",
       "http://127.0.0.1:5176",
+      "https://book-my-mandap-provider.vercel.app",
+      "https://admin-bookmymandap.vercel.app",
+      "https://bookmymandap.vercel.app"
     ],
     methods: ["GET", "PUT", "POST", "DELETE"],
     allowedHeaders: [
@@ -68,6 +71,9 @@ const io = socketIO(server, {
       "http://127.0.0.1:5174",
       "http://127.0.0.1:5175",
       "http://127.0.0.1:5176",
+        "https://book-my-mandap-provider.vercel.app",
+      "https://admin-bookmymandap.vercel.app",
+      "https://bookmymandap.vercel.app"
     ],
     methods: ["GET", "PUT", "POST", "DELETE"],
     allowedHeaders: [
